12135991156e8081c517bd203cc61c2630f153da
Dmytro Zharii <dima@zharii.com>
2020-10-05 21:13:55 -0700

    adjust font size

--- src/bookmarklets/clock.ts
+++ src/bookmarklets/clock.ts
@@ -8,14 +8,18 @@ container.setAttribute('style', `
     top: 0px;
     left: 0px;
     z-index: 2147483647;
-    color: green;
+    color: silver;
+    letter-spacing: 3px;
     background: black;
     font-family: Arial, Verdana;
-    font-size: 14pt;
+    font-size: 16pt;
+    padding-right: 10px;
+    font-weight: bold;
 
 `);
 
 document.body.appendChild(container);
+updateClock();
 setInterval(updateClock, 1000);
 
 function updateClock() {
