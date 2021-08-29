81321e7a04b8d515723cabbdb4c10263a65da6fd
Dmytro Zharii <dima@zharii.com>
2021-08-29 15:58:29 -0700

    add unsaved changes

--- src/bookmarklets/copy-page-title-md.ts
+++ src/bookmarklets/copy-page-title-md.ts
@@ -1,7 +1,7 @@
 import { copyToClipboard } from '../shared/clipboard';
 
 
-const title = document.title.replace(/[\[\(\]\)]/g, ' ');
+const title = document.title.replace(/[\|\[\(\]\)]/g, ' ');
 const url = document.location.href;
 
 copyToClipboard(`[${title}](${url})`, 'Copy Page URL to Clipboard', document);
